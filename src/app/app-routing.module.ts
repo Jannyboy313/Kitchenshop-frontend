04c185bcfe3b74d2c366e27f80776bf1e5ff6c53
Jan <s1120303@student.hsleiden.nl>
2020-12-28 14:19:59 +0100

    Added homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home/home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '/home', component: HomePageComponent },
 ];
 
 @NgModule({
