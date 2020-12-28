b8f34a0f9809ce8044164eaf4a2c395b3f5678f7
Jan <s1120303@student.hsleiden.nl>
2020-12-28 13:14:31 +0100

    Added module with navigationbar

---
+++ src/app/global/global.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+
+
+
+@NgModule({
+  declarations: [NavigationbarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GlobalModule { }
