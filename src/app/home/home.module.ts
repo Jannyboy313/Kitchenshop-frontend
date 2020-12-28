1e8f3ac318bc7177b05aeb0fe1c2682b9a7b2d03
Jan <s1120303@student.hsleiden.nl>
2020-12-28 16:55:05 +0100

    Added carousel

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { HomePageComponent } from './home-page/home-page.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, CarouselComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ]
 })
 export class HomeModule { }
