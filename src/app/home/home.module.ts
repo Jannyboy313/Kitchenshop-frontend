140f41de06f2a1604ecc896ccb2d91a651a1d598
Jan <s1120303@student.hsleiden.nl>
2020-12-28 17:54:42 +0100

    Added new component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { HotProductsComponent } from './hot-products/hot-products.component';
 
 @NgModule({
-  declarations: [CarouselComponent, HomePageComponent],
+  declarations: [CarouselComponent, HomePageComponent, HotProductsComponent],
   imports: [
     CommonModule,
     NgbModule
