0fd8298f98b209929ae9de83d731bff41a248a19
Jan <s1120303@student.hsleiden.nl>
2020-12-28 15:55:12 +0100

    Added footer component

--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [NavigationbarComponent],
+  declarations: [NavigationbarComponent, FooterComponent],
   imports: [
     CommonModule,
     NgbModule
