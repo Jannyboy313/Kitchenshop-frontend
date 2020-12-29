87d4fc23b2561fb2f5de2123590c90796913515a
Jan <s1120303@student.hsleiden.nl>
2020-12-29 22:52:54 +0100

    Created register form html/css

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,14 +11,23 @@ import { NgForm } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   isError = false;
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-    /// TODO: Please remove the timeout and fix it
-    onSubmit(form: NgForm) {
-      // doe iets
-    }
+  /// TODO: Please remove the timeout and fix it
+  onSubmit(form: NgForm) {
+    if(!this.authService.login({email: form.value.email, password: form.value.password})) {
+      setTimeout(() => {
+        this.isError = true;
+        form.reset();
+      }, 500)
 
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['/login']);
+  }
 }
