2c26e41b3d715a834ed9bc06dc198d5d6ab5ab14
Jan <s1120303@student.hsleiden.nl>
2020-12-29 12:41:18 +0100

    Added categories component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { HotProductsComponent } from './hot-products/hot-products.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
-  declarations: [CarouselComponent, HomePageComponent, HotProductsComponent],
+  declarations: [CarouselComponent, HomePageComponent, HotProductsComponent, CategoriesComponent],
   imports: [
     CommonModule,
     NgbModule
