9f617c8d82843bedd34514ab39afd20bcede1244
Jan <s1120303@student.hsleiden.nl>
2020-12-29 15:12:40 +0100

    Created getRequest for products

--- src/app/services/get.service.ts
+++ src/app/services/get.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Category } from '../models/category.model';
+import { Product } from '../models/product.model';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -17,9 +18,9 @@ export class GetService {
     map(data => data.map(data => new Category().deserialize(data))));
   }
 
-  public getProducts(): Observable<Category[]> {
+  public getProducts(): Observable<Product[]> {
     return this.http
-    .get<Category[]>('http://188.166.60.11/categories').pipe(
-    map(data => data.map(data => new Category().deserialize(data))));
+    .get<Product[]>('http://188.166.60.11/products').pipe(
+    map(data => data.map(data => new Product().deserialize(data))));
   }
 }
