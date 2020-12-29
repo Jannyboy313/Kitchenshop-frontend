6c89890fa88810f4a472aab6a173c3a4a6baf7bf
Jan <s1120303@student.hsleiden.nl>
2020-12-29 18:33:19 +0100

    Copy paste of loginscreen in register

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  isError = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+    /// TODO: Please remove the timeout and fix it
+    onSubmit(form: NgForm) {
+      // doe iets
+    }
 
 }
