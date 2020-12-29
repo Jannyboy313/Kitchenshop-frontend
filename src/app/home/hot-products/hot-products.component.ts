c5941d874ec60e89832e8cbeaef1ccff3f798277
Jan <s1120303@student.hsleiden.nl>
2020-12-29 16:00:48 +0100

    Made fully responsive

--- src/app/home/hot-products/hot-products.component.ts
+++ src/app/home/hot-products/hot-products.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hot-products.component.css']
 })
 export class HotProductsComponent implements OnInit {
+  hotProducts = [
+    {
+      "name": "Kenwood machine",
+      "price": "10.99",
+      "description": "THIS IS AMAZING",
+      "image": null
+    },
+    {
+      "name": "Bosch machine",
+      "price": "109.99",
+      "description": "THIS IS AMAZING",
+      "image": null
+    },
+    {
+      "name": "KitchenAid machine",
+      "price": "999.99",
+      "description": "THIS IS AMAZING",
+      "image": null
+    },
+  ]
 
   constructor() { }
 
