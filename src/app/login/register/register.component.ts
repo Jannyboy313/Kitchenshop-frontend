6b883b6f0c8cb54a9899b75b9f2c76d429a876e0
Jan <s1120303@student.hsleiden.nl>
2020-12-30 12:07:26 +0100

    Fixed validation and removed ngmodel

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -20,21 +20,21 @@ export class RegisterComponent implements OnInit {
 
   initForm(): void {
     this.registerForm = this.formBuilder.group({
-      email: ['', [Validators.required,
-      Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')]],
-      password: ['', Validators.required],
       firstName: ['', [Validators.required,
-      Validators.pattern('([A-Z][a-zA-Z]*)')]],
+        Validators.pattern('([A-Z][a-zA-Z]*)')]],
       middleName: ['', Validators.pattern('([A-Z][a-zA-Z]*)')],
       lastName: ['', [Validators.required,
-      Validators.pattern('([A-Z][a-zA-Z]*)')]]
+        Validators.pattern('([A-Z][a-zA-Z]*)')]],
+      email: ['', [Validators.required,
+        Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')]],
+      password: ['', Validators.required],
     });
   }
 
   isInValidInput(fieldName): boolean {
     return this.registerForm.controls[fieldName].invalid &&
       (this.registerForm.controls[fieldName].dirty || this.registerForm.controls[fieldName].touched);
-}
+  }
 
   // TODO: Please remove the timeout and fix it
   onSubmit() {
