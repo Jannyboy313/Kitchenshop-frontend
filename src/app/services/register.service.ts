26f8241a27e2b0e69a33281aafeb004341b16e1a
Jan <s1120303@student.hsleiden.nl>
2020-12-31 11:21:38 +0100

    Created register function

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from "../../environments/environment";
+import { AuthService } from './auth.service';
+
+import { User } from '../models/user.model';
+import { Address } from '../models/address.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router, private authService: AuthService) {}
+
+  register(user: User, address: Address) : any {
+    let status: {"isSuccess": boolean, "error": string};
+    this.http.post<any>(environment.apiUrl + "/register",
+    {"user": user, "address": address})
+      .subscribe(
+        (value) => {
+          this.router.navigate(['/login']);
+          status.isSuccess = true;
+        },
+        response => {
+          console.log("err", response);
+          status.isSuccess = false;
+          status.error = response.error;
+        }
+      );
+      return status
+  }
 }
