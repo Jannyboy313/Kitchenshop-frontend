bcd6eec813a123f0704ea628da05366f884d2b80
Jan <s1120303@student.hsleiden.nl>
2021-01-02 11:30:07 +0100

    Added functions to service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  productsInCart = [];
 
   constructor() { }
+
+  addProduct(product: Product) {
+    this.productsInCart.push(product);
+  }
+
+  removeProduct(product: Product) {
+    this.productsInCart.forEach((item, index) => {
+      if (product.productnumber === item.productnumber) {
+        this.productsInCart.splice(index, 1);
+        return;
+      }
+    })
+  }
+
+  clearCart() {
+    this.productsInCart = [];
+  }
 }
