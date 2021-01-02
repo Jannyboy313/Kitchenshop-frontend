5b77fb2276a627f8e2f1c7ba6a9b7ca9b199fa42
Jan <s1120303@student.hsleiden.nl>
2021-01-02 13:47:10 +0100

    Added functions for receiving the data array

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -1,4 +1,7 @@
+import { NonNullAssert } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  itemsArray: Product[] = [];
+  filteredItemsArray: Product[] = [];
+  amountItems = new Map();
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.itemsArray = this.cartService.getItems();
+    this.getAmountItems();
+    this.removeDuplicates()
+  }
+
+  getAmountItems() {
+    let itemsMap = new Map();
+    this.itemsArray.forEach((item) => {
+      if (itemsMap.has(item.productnumber)) {
+        let count = itemsMap.get(item.productnumber) + 1;
+        itemsMap.set(item.productnumber, count);
+      } else {
+        itemsMap.set(item.productnumber, 1);
+      }
+    })
+  }
+
+  removeDuplicates() {
+    this.itemsArray.forEach((item) => {
+      let exists = 0;
+      console.log(item)
+      this.filteredItemsArray.forEach(element => {
+        if (element.productnumber === item.productnumber) {
+          exists++;
+        }
+      });
+      if (exists === 0) {
+        this.filteredItemsArray.push(item);
+      }
+    })
   }
 
 }
