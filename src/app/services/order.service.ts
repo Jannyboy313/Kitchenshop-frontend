edbfbab8e0325ba57453be813dfc334ad476aa11
Jannyboy313 <janvanoverbeek12@gmail.com>
2021-01-02 21:21:50 +0100

    Generated order model

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  addOrders(orders: any[]) : any {
+    return this.http.post<any>(environment.apiUrl + "/addorders", orders);
+  }
 }
