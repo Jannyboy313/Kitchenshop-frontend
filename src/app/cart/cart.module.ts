ed2bf04b30cbc5119f61728b06622e0c11f10250
Jan <s1120303@student.hsleiden.nl>
2021-01-02 11:34:49 +0100

    Added cart-item component

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 
 
 @NgModule({
-  declarations: [CartComponent],
+  declarations: [CartComponent, CartItemComponent],
   imports: [
     CommonModule
   ]
