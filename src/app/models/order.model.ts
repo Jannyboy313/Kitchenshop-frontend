2e01d988c5cbe64310059019e1f376a7fa94cc9c
Jannyboy313 <janvanoverbeek12@gmail.com>
2021-01-02 21:24:13 +0100

    Finished order model

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -1,8 +1,10 @@
 import { Deserializable } from "./deserializable.model";
 
 export class Order implements Deserializable {
-  name: string;
-  description: string;
+  orders_id: number = null;
+  timestamp: string = null;
+  user_id: number;
+  productnumber: number;
 
   deserialize(input: any) {
     Object.assign(this, input);
