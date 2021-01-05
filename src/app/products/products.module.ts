339bcff30e8a6b109ec8e87976f834327eec9a9d
Jan <s1120303@student.hsleiden.nl>
2021-01-05 15:22:57 +0100

    Added model for product info

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductPageComponent } from './product-page/product-page.component';
-
-
+import { ProductInfoModalComponent } from './product-info-modal/product-info-modal.component';
 
 @NgModule({
-  declarations: [ProductPageComponent],
+  declarations: [ProductPageComponent, ProductInfoModalComponent],
   imports: [
     CommonModule
   ]
