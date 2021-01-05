8ef027f3dab7f6fbbe7bff8f23771ff51b5f3f28
Jan <s1120303@student.hsleiden.nl>
2021-01-05 17:38:28 +0100

    Added order

--- src/app/orders/user-orders/user-orders.component.ts
+++ src/app/orders/user-orders/user-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/models/order.model';
 
 @Component({
   selector: 'app-user-orders',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserOrdersComponent implements OnInit {
 
+  orders: Order[];
   constructor() { }
 
   ngOnInit(): void {
