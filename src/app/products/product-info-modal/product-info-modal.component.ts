4c33300b9dd7472aec3b6a861bfe652c51e84dfc
Jan <s1120303@student.hsleiden.nl>
2021-01-05 15:27:34 +0100

    Added template

--- src/app/products/product-info-modal/product-info-modal.component.ts
+++ src/app/products/product-info-modal/product-info-modal.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, OnDestroy } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { ModalService } from 'src/app/services/modal.service';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-info-modal',
   templateUrl: './product-info-modal.component.html',
   styleUrls: ['./product-info-modal.component.css']
 })
-export class ProductInfoModalComponent implements OnInit {
+export class ProductInfoModalComponent implements OnInit, OnDestroy {
+  @Input() id: string;
+  @Input() product: Product;
+  private element: any;
+  isShown = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private modalService: ModalService,
+     private el: ElementRef,
+     private cartService: CartService,) {
+      this.element = this.el.nativeElement;
   }
 
+  ngOnInit(): void {
+      if (!this.id) {
+          console.error('modal must have an id');
+          return;
+      }
+      document.body.appendChild(this.element);
+      this.modalService.add(this);
+  }
+
+  ngOnDestroy(): void {
+    this.modalService.remove(this.id);
+    this.element.remove();
+  }
+
+  open(): void {
+    this.isShown = true;
+  }
+
+  close(): void {
+    this.isShown = false;
+  }
+
+  getMoneyAmount(): number{
+    return this.cartService.getTotalPrice();
+  }
 }
