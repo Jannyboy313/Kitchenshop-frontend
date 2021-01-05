28dbf0bfa5eaea471992187a2c0288d05eecc3b0
Jan <s1120303@student.hsleiden.nl>
2021-01-05 17:29:32 +0100

    Created user orders

---
+++ src/app/orders/orders.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserOrdersComponent } from './user-orders/user-orders.component';
+
+
+
+@NgModule({
+  declarations: [UserOrdersComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrdersModule { }
