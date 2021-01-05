ec8b4b6f68e7c6e2084ff509766fd5e52fc3cb78
Jan <s1120303@student.hsleiden.nl>
2021-01-05 12:55:53 +0100

    Added path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { CartComponent } from './cart/cart/cart.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { LoginComponent } from './login/login/login.component';
 import { RegisterComponent } from './login/register/register.component';
+import { ProductPageComponent } from './products/product-page/product-page.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  { path: 'products', component: ProductPageComponent}
 ];
 
 @NgModule({
