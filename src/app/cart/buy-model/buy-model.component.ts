ede4bbe48cbaa8e10dc5b0944ac9a8e1e40306e2
Jan <s1120303@student.hsleiden.nl>
2021-01-05 08:13:32 +0100

    Generated buy modal

---
+++ src/app/cart/buy-model/buy-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-model',
+  templateUrl: './buy-model.component.html',
+  styleUrls: ['./buy-model.component.css']
+})
+export class BuyModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
+import { BuyModelComponent } from './buy-model/buy-model.component';
 
 
 @NgModule({
-  declarations: [CartComponent],
+  declarations: [CartComponent, BuyModelComponent],
   imports: [
     CommonModule
   ]
