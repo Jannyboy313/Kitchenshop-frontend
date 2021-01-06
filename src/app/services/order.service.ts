4fa46424e777788801621b5a6273b771ef33ec4f
Jan <s1120303@student.hsleiden.nl>
2021-01-06 08:06:57 +0100

    Added http req for customer orders

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from "../../environments/environment";
 import { Order } from '../models/order.model';
+import { Product } from '../models/product.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,11 @@ export class OrderService {
 
   addOrders(orders: Order[]) : any {
     return this.http.post<any>(environment.apiUrl + "/addorders", orders);
+  }
+
+  getCustomerOrders() {
+    return this.http
+    .get<any>(`${environment.apiUrl}/customerorders`).pipe(
+    map(data => data.map(data => new Order().deserialize(data), new Product().deserialize(data))));
   }
 }
