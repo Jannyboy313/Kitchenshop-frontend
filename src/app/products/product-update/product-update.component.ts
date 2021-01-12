bd394933b07c97828d947e13de3fbf26583f140a
Jan <s1120303@student.hsleiden.nl>
2021-01-12 16:26:52 +0100

    Added products

--- src/app/products/product-update/product-update.component.ts
+++ src/app/products/product-update/product-update.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-update',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = []
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  deleteButton(product) {
+
+  }
+
+  updateButton(product) {
+
+  }
+
+  private getProducts() {
+    this.productsService.getProducts().subscribe(products => this.products = products);
+  }
+
+
+  getShortenedDescription(description: string) {
+    let short = description.split(" ");
+    let shortenedDescription: string = '';
+    for (let i=0; i<short.length; i++) {
+      if (i <= 25) {
+        shortenedDescription += ' ' + short[i]
+      }else {
+        return shortenedDescription.trim();
+      }
+    }
+  }
 }
