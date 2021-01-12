843e94486aec7ba635c3e2bec87cf0591d3547e0
Jan <s1120303@student.hsleiden.nl>
2021-01-12 11:48:49 +0100

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,18 @@ import { RegisterComponent } from './login/register/register.component';
 import { ProductPageComponent } from './products/product-page/product-page.component'
 import { UserOrdersComponent } from './orders/user-orders/user-orders.component';
 import { AdminOrdersComponent } from './orders/admin-orders/admin-orders.component';
+import { ProductCreateComponent } from './products/product-create/product-create.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/adminorders', pathMatch: 'full' },
+  { path: '', redirectTo: '/productcreate', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'cart', component: CartComponent },
   { path: 'products', component: ProductPageComponent},
   { path: 'customerorders', component: UserOrdersComponent},
-  { path: 'adminorders', component: AdminOrdersComponent}
+  { path: 'adminorders', component: AdminOrdersComponent},
+  { path: 'productcreate', component: ProductCreateComponent}
 ];
 
 @NgModule({
