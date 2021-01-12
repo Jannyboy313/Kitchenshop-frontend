00ec2f13a7de42093b9ca7c9d2cf220b60c001de
Jan <s1120303@student.hsleiden.nl>
2021-01-12 10:24:07 +0100

    Removed whitespace

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { UserOrdersComponent } from './user-orders/user-orders.component';
 import { AdminOrdersComponent } from './admin-orders/admin-orders.component';
 
-
-
 @NgModule({
   declarations: [UserOrdersComponent, AdminOrdersComponent],
   imports: [
