5a1a77fb55d82b93557e1eb9818db44f138f255c
Jan <s1120303@student.hsleiden.nl>
2021-01-12 09:38:54 +0100

    Added functions for sorting orders on timestamp

--- src/app/orders/user-orders/user-orders.component.ts
+++ src/app/orders/user-orders/user-orders.component.ts
@@ -8,8 +8,7 @@ import { OrderService } from '../../services/order.service';
   styleUrls: ['./user-orders.component.css']
 })
 export class UserOrdersComponent implements OnInit {
-
-  orders: Order[];
+  orders = new Map<string, Order[]>();
   constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
@@ -18,7 +17,46 @@ export class UserOrdersComponent implements OnInit {
 
   getCustomerOrders() {
     this.orderService.getCustomerOrders()
-      .subscribe(orders => this.orders = orders)
+      .subscribe(orders => {
+        this.orders = this.ordersByDate(orders);
+        this.ordersByAmount();
+      });
+  }
+
+  private ordersByDate(orders) {
+    let sortedOrders = new Map<string, Order[]>();
+    for (let i=0; i < orders.length; i++) {
+      if (!sortedOrders.has(orders[i].timestamp)) {
+        sortedOrders.set(orders[i].timestamp, [])
+      }
+      sortedOrders.get(orders[i].timestamp).push(orders[i]);
+    }
+    return sortedOrders;
+  }
+
+  private ordersByAmount() {
+    this.orders.forEach((value, key) => {
+      for(let i=0; i < this.orders.get(key).length; i++) {
+        let exists = 0;
+        for(let j=0; j < this.orders.get(key).length; j++) {
+          if (this.orders.get(key)[i].productnumber === this.orders.get(key)[j].productnumber) {
+            exists++
+          }
+          if (exists >=2) {
+            this.orders.get(key).splice(j, 1);
+          }
+        }
+        this.orders.get(key)[i].amount = exists;
+        this.orders.get(key)[i].price = String(this.roundToTwoDecimals(this.orders.get(key)[i].price) * exists);
+      }
+    });
+  }
+
+  private roundToTwoDecimals(string) {
+    let number = parseFloat(string);
+    number = number * 100;
+    number = Math.trunc(number);
+    return number / 100;
   }
 
 }
