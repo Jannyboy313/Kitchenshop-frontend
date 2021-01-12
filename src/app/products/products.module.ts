05010c7f7eb2fd6584e8c29a51a789c09716b8ca
Jan <s1120303@student.hsleiden.nl>
2021-01-12 11:46:17 +0100

    Generated product create page

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductInfoModalComponent } from './product-info-modal/product-info-modal.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
 
 @NgModule({
-  declarations: [ProductPageComponent, ProductInfoModalComponent],
+  declarations: [ProductPageComponent, ProductInfoModalComponent, ProductCreateComponent],
   imports: [
     CommonModule
   ]
