a8387a0d78b9e319b50a5e334cc7b8e640086dfc
Jan <s1120303@student.hsleiden.nl>
2021-01-12 16:14:06 +0100

    Generated product update

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,9 +4,10 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductInfoModalComponent } from './product-info-modal/product-info-modal.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductUpdateComponent } from './product-update/product-update.component';
 
 @NgModule({
-  declarations: [ProductPageComponent, ProductInfoModalComponent, ProductCreateComponent],
+  declarations: [ProductPageComponent, ProductInfoModalComponent, ProductCreateComponent, ProductUpdateComponent],
   imports: [
     CommonModule,
     FormsModule,
