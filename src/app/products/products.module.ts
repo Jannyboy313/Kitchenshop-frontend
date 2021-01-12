1d71fcd2bdcfd99b8b69485188642569ce38a10b
Jan <s1120303@student.hsleiden.nl>
2021-01-12 14:25:59 +0100

    Added form module imports

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductInfoModalComponent } from './product-info-modal/product-info-modal.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProductPageComponent, ProductInfoModalComponent, ProductCreateComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
