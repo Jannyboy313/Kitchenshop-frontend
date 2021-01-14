1b05d29ba6a039bde4befdfdc2e7eda43c6b62f0
Jan <s1120303@student.hsleiden.nl>
2021-01-14 08:37:15 +0100

    Added observable to component

--- src/app/users/users/users.component.ts
+++ src/app/users/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { UsersService } from '../../services/users.service';
 
@@ -9,10 +10,18 @@ import { UsersService } from '../../services/users.service';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  usersObservable$: Observable<User[]>;
 
   constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.usersService.getUsers()
+      .subscribe(users => {
+        this.usersObservable$ = this.usersService.getUsers();
+        this.usersObservable$.subscribe(updatedUsers => {
+          this.users = updatedUsers;
+        })
+      })
   }
 
 }
