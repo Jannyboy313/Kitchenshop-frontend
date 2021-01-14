da7dbf5e41eceaf363ffc111e4bca36999dc691c
Jan <s1120303@student.hsleiden.nl>
2021-01-14 08:31:41 +0100

    Added set role function

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -19,8 +19,18 @@ export class UsersService {
 
   deleteUser(user) {
     for (let i=0; i<this.users.length; i++) {
-      if (this.users[i].user_id == user.id) {
+      if (this.users[i].user_id == user.user_id) {
         this.users.splice(i, 1);
+        break;
+      }
+    }
+    this.usersSubject.next(this.users);
+  }
+
+  setRole(user) {
+    for (let i=0; i<this.users.length; i++) {
+      if (this.users[i].user_id == user.user_id) {
+        this.users[i].role = user.role
         break;
       }
     }
