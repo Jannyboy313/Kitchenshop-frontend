ebc39577ae54fa552c20c56d97299b966641413a
Jan <s1120303@student.hsleiden.nl>
2021-01-14 08:27:51 +0100

    Added delete function

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  users: User[];
+  private users: User[];
+  private userSubject = new Subject<User[]>();
 
-  constructor() { }
+  constructor() {
+    this.userSubject.next([]);
+  }
+
+  deleteUser(user) {
+    for (let i=0; i<this.users.length; i++) {
+      if (this.users[i].user_id == user.id) {
+        this.users.splice(i, 1);
+        break;
+      }
+    }
+    this.userSubject.next(this.users);
+  }
 }
