dbd3f0c486557c8f3ca03725520204b1a35cd610
Jan <s1120303@student.hsleiden.nl>
2021-01-14 08:04:26 +0100

    Added users

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  users: User[];
 
   constructor() { }
 }
