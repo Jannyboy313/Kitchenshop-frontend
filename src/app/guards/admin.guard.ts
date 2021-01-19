fc6b4ea987c0c9b796b48d8440f186020420e372
Jan <s1120303@student.hsleiden.nl>
2021-01-19 14:46:58 +0100

    Enabled guards

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -11,15 +11,13 @@ export class AdminGuard implements CanActivate {
   constructor(private authService: AuthService) {}
 
   canActivate (
-    //TODO Enable when out of production
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      // if (this.authService.getUser().permission === 'admin') {
-      //   return true;
-      // }
-      // window.alert('You don\'t have permission to view this page');
-      // return false;
-      return true;
+      if (this.authService.getUser().permission === 'admin') {
+        return true;
+      }
+      window.alert('You don\'t have permission to view this page');
+      return false;
   }
 
 }
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -11,15 +11,13 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService) {}
 
   canActivate (
-    //TODO Enable when out of production
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      // if (this.authService.isLoggedIn()) {
-      //   return true;
-      // }
-      // window.alert('You need to be logged in to view this page');
-      // return false;
-      return true;
+      if (this.authService.isLoggedIn()) {
+        return true;
+      }
+      window.alert('You need to be logged in to view this page');
+      return false;
   }
 
 }
